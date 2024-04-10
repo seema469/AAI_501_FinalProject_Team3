abe0ddc2f10e6951889dd95f949587f7b373b364
seema469 <seema469.mittal@gmail.com>
2024-04-10 16:13:49 -0700

    Updated- Three splits to combine as single file not 4. Remove train_split4.zip
    Updated- Three splits to combine as single file not 4. Remove train_split4.zip

--- dataset/CombineDataSet.py
+++ dataset/CombineDataSet.py
@@ -7,8 +7,8 @@ import numpy as np
 split1 = np.load('train_split1.npy')
 split2 = np.load('train_split2.npy')
 split3 = np.load('train_split3.npy')
-split4 = np.load('train_split4.npy')
 
-combined_data = np.concatenate((split1, split2, split3, split4))
+
+combined_data = np.concatenate((split1, split2, split3))
 
 np.save("train_images_singleImages.npy", combined_data)
